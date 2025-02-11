86258dc46d1f322b45e0094cdd2ae3b80aea0b2e
Duleetha Madubhashana <148762717+Duleetha2@users.noreply.github.com>
2025-02-11 17:05:42 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName;
+            string lastName;
+            int age;
+            double weight;
+
+            Console.Write("Enter Your First Name:");
+            firstName = Console.ReadLine();
+
+            Console.Write("Enter Your last Name:");
+            lastName = Console.ReadLine();
+
+            Console.Write("Enter Your Age:");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Your Weight:");
+            weight = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Your Name is :" + firstName + " " + lastName);
+            Console.WriteLine("Your Age is :" + age + " " + "Years old");
+            Console.WriteLine("Your weight is :" + weight);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
